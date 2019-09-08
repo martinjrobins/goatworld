a1a91e6ddcdd8bee608e8cd569235d564c301b3c
Martin Robinson <martinjrobins@gmail.com>
2019-09-08 09:44:40 +0100

    Updates

---
+++ precache-manifest.2b38b61dd0ddbbae16799958cecd6b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c71e7bcbb4af22d709d64a4484e793",
+    "url": "/goatworld/index.html"
+  },
+  {
+    "revision": "ac3b041cfa204dfaddba",
+    "url": "/goatworld/static/css/main.6f637483.chunk.css"
+  },
+  {
+    "revision": "7bc60db9d5ed1c280609",
+    "url": "/goatworld/static/js/2.b0fe009c.chunk.js"
+  },
+  {
+    "revision": "ac3b041cfa204dfaddba",
+    "url": "/goatworld/static/js/main.0344734c.chunk.js"
+  },
+  {
+    "revision": "c9d2a1824cd3573b9535",
+    "url": "/goatworld/static/js/runtime~main.de8883c1.js"
+  }
+]);
